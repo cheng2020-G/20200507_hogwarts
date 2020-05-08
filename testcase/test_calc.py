55b6dcc25c8477d3db9f205fab2612e3595ecd42
cheng2020-G <myautotest123698745>
2020-05-08 19:55:12 +0800

    commit:alter datafile path

--- testcase/test_calc.py
+++ testcase/test_calc.py
@@ -9,12 +9,12 @@ class TestCalc:
         self.calc = Calc()
 
     # yaml参数化
-    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507/data/data.yaml")))
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data.yaml")))
     def test_add(self, a, b, c):
         result = self.calc.add(a, b)
         assert result == c
 
-    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507/data/data1.yaml")))
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data1.yaml")))
     def test_div(self, a, b, c):
         result = self.calc.div(a, b)
         assert result == c

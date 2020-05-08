a0930350d072882621feb4efe11121181936851b
cheng2020-G <myautotest123698745>
2020-05-08 19:43:42 +0800

    push calc

---
+++ testcase/test_calc.py
@@ -0,0 +1,24 @@
+import pytest
+import yaml
+
+from unit.calc import Calc
+
+
+class TestCalc:
+    def test_step(self):
+        self.calc = Calc()
+
+    # yaml参数化
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507/data/data.yaml")))
+    def test_add(self, a, b, c):
+        result = self.calc.add(a, b)
+        assert result == c
+
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507/data/data1.yaml")))
+    def test_div(self, a, b, c):
+        result = self.calc.div(a, b)
+        assert result == c
+
+
+if __name__ == '__main__':
+    pytest.main()

52ddba193b66eeb6da8a144213be33401a38fec6
cheng2020-G <myautotest123698745>
2020-05-08 23:00:52 +0800

    add testreport

--- testcase/test_calc.py
+++ testcase/test_calc.py
@@ -1,20 +1,25 @@
 import pytest
 import yaml
-
+import allure
 from unit.calc import Calc
 
 
 class TestCalc:
+    @allure.feature("计算器")
     def setup(self):
         self.calc = Calc()
 
     # yaml参数化
-    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data.yaml")))
+    @allure.story("加法")
+    @pytest.mark.parametrize(["a", "b", "c"],
+                             yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data.yaml")))
     def test_add(self, a, b, c):
         result = self.calc.add(a, b)
         assert result == c
 
-    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data1.yaml")))
+    @allure.story("除法")
+    @pytest.mark.parametrize(["a", "b", "c"],
+                             yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data1.yaml")))
     def test_div(self, a, b, c):
         result = self.calc.div(a, b)
         assert result == c

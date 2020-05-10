29a23f115304e391fc2a1ab65b5327ff7ac5eaaa
cheng2020-G <myautotest123698745>
2020-05-10 13:30:35 +0800

    add test_sub and test_mul and datafile

--- testcase/test_calc.py
+++ testcase/test_calc.py
@@ -12,14 +12,28 @@ class TestCalc:
     # yaml参数化
     @allure.story("加法")
     @pytest.mark.parametrize(["a", "b", "c"],
-                             yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data.yaml")))
+                             yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data_add.yaml")))
     def test_add(self, a, b, c):
+        # try:
         result = self.calc.add(a, b)
+        assert result == c
+        # except:
+
+    @allure.story("减法")
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data_sub.yaml")))
+    def test_sub(self, a, b, c):
+        result = self.calc.sub(a, b)
+        assert result == c
+
+    @allure.story("乘法")
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data_mul.yaml")))
+    def test_mul(self, a, b, c):
+        result = self.calc.mul(a, b)
         assert result == c
 
     @allure.story("除法")
     @pytest.mark.parametrize(["a", "b", "c"],
-                             yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data1.yaml")))
+                             yaml.safe_load(open("D:/PycharmProject/20200507_pytest_calc/data/data_div.yaml")))
     def test_div(self, a, b, c):
         result = self.calc.div(a, b)
         assert result == c

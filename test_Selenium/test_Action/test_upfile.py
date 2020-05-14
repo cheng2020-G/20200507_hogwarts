289f20357dd5bb1307714154febe57f29bf5e49b
cheng2020-G <myautotest123698745>
2020-05-14 12:40:08 +0800

    add image

--- test_Selenium/test_Action/test_upfile.py
+++ test_Selenium/test_Action/test_upfile.py
@@ -7,5 +7,5 @@ class TestUpFile(TestAction):
     def test_up_file(self):
         self.driver.get("https://image.baidu.com")
         self.driver.find_element_by_xpath("//*[@id='sttb']//img[1]").click()
-        self.driver.find_element_by_id("stfile").send_keys("E:/1.jpp")
+        self.driver.find_element_by_id("stfile").send_keys("D:\PycharmProject\20200507_pytest_calc\image\1.jpg")
         sleep(3)

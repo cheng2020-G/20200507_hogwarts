c7ce32daf411372061f3d4fa0ad4a371852985a5
cheng2020-G <myautotest123698745>
2020-05-22 17:54:51 +0800

    修复assert

--- selenium_wechat_po/page/add_member.py
+++ selenium_wechat_po/page/add_member.py
@@ -11,7 +11,7 @@ class AddMember(BasePage):
         self.find(By.ID, "memberAdd_acctid").send_keys("20200521")
         self.find(By.ID, "memberAdd_phone").send_keys("15800000000")
         self.find(By.CSS_SELECTOR, ".js_btn_save").click()
-        # self.driver.quit()
+        self.wait_for_click((By.CSS_SELECTOR, ".js_has_member>div:nth-child(1) .js_add_member"))
 
     def get_element(self):
         elements = self.finds(By.CSS_SELECTOR, ".member_colRight_memberTable_td:nth-child(2)")

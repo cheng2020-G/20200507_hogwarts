b15fc88b39e9b8ef3c7806950ed7f9b1ef79cf8c
cheng2020-G <myautotest123698745>
2020-06-23 12:20:55 +0800

    完善jsonpath提取及相应断言

--- test_requests/test_json_path.py
+++ test_requests/test_json_path.py
@@ -7,20 +7,23 @@ class TestJsonpath():
     def test_jsonpath1(self):
         data = {'hogwarts': ["a", "b", "c"]}
         r = requests.post('http://httpbin.org/post', data=data)
-        url = jsonpath(r.json(), '$["url"]')
+        url = jsonpath(r.json(), '$..url')[0]
         print(r.text)
-        print('url is:' + str(url))
+        print('url is:' + url)
 
     def test_jsonpath2(self):
         r = requests.get('https://testerhome.com/api/v3/topics.json?limit=2')
-        user_id = jsonpath(r.json(), '$..topics.user[?(@.user.login)]')
+        # user_id = jsonpath(r.json(), '$..topics.user[?(@.user.login)]')
         print(r.text)
-        print('url is:' + str(user_id))
+        print(jsonpath(r.json(), '$..user'))
+        user_id = jsonpath(r.json(), '$..user.login')
+        print(user_id)
 
     def test_hogwarts_jsonpath(self):
         url = "https://ceshiren.com/categories.json"
         r = requests.get(url)
         print(r.json())
         assert r.json()['category_list']['categories'][0]['name'] == '霍格沃兹测试学院公众号'
-        assert jsonpath(r.json(), '$.category_list.categories[0].name') == '霍格沃兹测试学院公众号'
+        # 使用jsonpath提取出的内容为一个列表，需要再次提取[0]
+        assert jsonpath(r.json(), '$.category_list.categories[0].name')[0] == '霍格沃兹测试学院公众号'
         assert jsonpath(r.json(), '$..name')[0] == '霍格沃兹测试学院公众号'
